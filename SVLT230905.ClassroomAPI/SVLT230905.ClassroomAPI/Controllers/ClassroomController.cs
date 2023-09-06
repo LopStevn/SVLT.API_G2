8516f3d14e0648c11f4071897d2369b0b5b8c7d2
LopStevn <stevenlpz03@gmail.com>
2023-09-05 19:37:37 -0600

    API de Aulas - Completado

---
+++ SVLT230905.ClassroomAPI/SVLT230905.ClassroomAPI/Controllers/ClassroomController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SVLT230905.ClassroomAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SVLT230905.ClassroomAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassroomController : ControllerBase
+    {
+        static List<Classroom> classrooms = new List<Classroom>
+        {
+            new Classroom { Id = 1, Name = "1ro Contador A", NumbersStud = 35, Teacher = "Juan Roman" },
+            new Classroom { Id = 2, Name = "2do General B", NumbersStud = 40, Teacher = "Maria Gomez" },
+            new Classroom { Id = 3, Name = "1ro Mecanica C", NumbersStud = 26, Teacher = "Gerard Moreno" },
+            new Classroom { Id = 4, Name = "1ro Software A", NumbersStud = 30, Teacher = "Angel Perez" },
+        };
+
+        // GET: api/<ClassroomController>
+        [HttpGet]
+        public IEnumerable<Classroom> Get()
+        {
+            return classrooms;
+        }
+
+        
+    }
+}
---
+++ SVLT230905.ClassroomAPI/SVLT230905.ClassroomAPI/Models/Classroom.cs
@@ -0,0 +1,13 @@
+﻿namespace SVLT230905.ClassroomAPI.Models
+{
+    public class Classroom
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int? NumbersStud { get; set; }
+
+        public string? Teacher { get; set; }
+    }
+}
