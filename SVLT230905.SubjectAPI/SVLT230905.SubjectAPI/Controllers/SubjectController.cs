50e145f296f6d1650ea992f2ea7f1a2806b2e203
LopStevn <stevenlpz03@gmail.com>
2023-09-05 19:15:22 -0600

    API Materias - Completada

---
+++ SVLT230905.SubjectAPI/SVLT230905.SubjectAPI/Controllers/SubjectController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SVLT230905.SubjectAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SVLT230905.SubjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        static List<Subject> subjects = new List<Subject>();
+
+        // GET: api/<SubjectController>
+        [HttpGet]
+        public IEnumerable<Subject> Get()
+        {
+            return subjects;
+        }
+
+        // POST api/<SubjectController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Subject subject)
+        {
+            subjects.Add(subject);
+            return Ok();
+        }
+
+        // DELETE api/<SubjectController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingSubject = subjects.FirstOrDefault(s => s.Id == id);
+            if (existingSubject != null)
+            {
+                subjects.Remove(existingSubject);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ SVLT230905.SubjectAPI/SVLT230905.SubjectAPI/Models/Subject.cs
@@ -0,0 +1,13 @@
+﻿namespace SVLT230905.SubjectAPI.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+
+        public string? SubjectName { get; set; }
+
+        public string? TeacherName { get; set; }
+
+        public string? Units { get; set; }
+    }
+}
